13e8f0929d0b2abcda4f6e00cbb4b06bddbc783f
FNUNES23 <flavianunes.dc@gmail.com>
2023-12-15 13:40:33 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>  
+using namespace std;
+
+int main () {
+//variable permettant de completer le nombre de places disponibles int nombreplace = 10;
+  
+//variable permettant de récupérer le choix de l'utilisateur int choix;
+
+  cout <<"************ Evaluation 2 ************" << endl << endl;
+  
+//partie 3
+
+  cout << "1 - Entrée dans le parking " << endl;
+  cout << "2 - Nombre de places disponibles " << endl;
+  cout << "3 - Sortie du parking " << endl;
+  cout << "4 - Quitter " << endl;
+  
+  return 0;
+}

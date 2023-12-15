61b4a3807d7e99d471727e5748e3f22d11a5ec5e
FNUNES23 <flavianunes.dc@gmail.com>
2023-12-15 14:04:57 +0000

    choix de l'utilisateur

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,36 @@
 using namespace std;
 
 int main () {
-//variable permettant de completer le nombre de places disponibles 
-  int nombreplace = 10;
-  
-//variable permettant de récupérer le choix de l'utilisateur 
-  int choix;
 
-  cout <<"************ Evaluation 2 ************" << endl << endl;
-  
-//partie 3
+  cout << "********** Evaluation 2 **********" << endl;
+    cout << endl;
+    cout << "1 - Entrée dans le parking" << endl;
+    cout << "2 - Nombre de places" << endl;
+    cout << "3 - Sortie du parking" << endl;
+    cout << "4 - Quitter" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "Saisir un nombre" << endl;
 
-  cout << "1 - Entrée dans le parking " << endl;
-  cout << "2 - Nombre de places disponibles " << endl;
-  cout << "3 - Sortie du parking " << endl;
-  cout << "4 - Quitter " << endl;
-  
-  return 0;
-}
+    int choix;
+
+    cin >> choix;
+
+    if (choix == 1) {
+      cout << "vous avez choisi Entrée dans le parking";
+    } else {
+      if (choix == 2) {
+        cout << "vous avez choisi Nombre de places";
+      } else {
+        if (choix == 3) {
+          cout << "vous avez choisi Sortie du parking";
+        } else {
+          if (choix == 4) {
+            cout << "vous avez choisi Quitter";
+          } else {
+            cout << "ERROR! Selectionnez 1 à 4";
+          }
+        }
+      }
+    }
+  }
